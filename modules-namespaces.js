b81973c07046be759f1123ed9a510cb3c2f7106e
timebandit <imran.nazir@yahoo.co.uk>
2018-12-06 11:18:26 +0000

    namespaces & modules

--- modules-namespaces.js
+++ modules-namespaces.js
@@ -1,1 +1,22 @@
 console.log("module & namespaces 👻");
+// namespaces are for grouping things, prevents polution
+// of the global scope
+// namespaces are imported with
+/*
+
+/// <reference path="circleMath.js">
+/// <reference path="rectangleMath.js">
+
+*/
+var MyMath;
+(function (MyMath) {
+    const PI = 3.14;
+    function calculateCircumference(diameter) {
+        return diameter * PI;
+    }
+    MyMath.calculateCircumference = calculateCircumference;
+    function calculateRectangle(width, height) {
+        return width * length;
+    }
+    MyMath.calculateRectangle = calculateRectangle;
+})(MyMath || (MyMath = {}));
--- modules-namespaces.ts
+++ modules-namespaces.ts
@@ -1,1 +1,23 @@
 console.log("module & namespaces 👻");
+// namespaces are for grouping things, prevents polution
+// of the global scope
+
+// namespaces are imported with
+/* 
+
+/// <reference path="circleMath.js">
+/// <reference path="rectangleMath.js">
+
+*/
+
+namespace MyMath {
+  const PI = 3.14;
+
+  export function calculateCircumference(diameter: number) {
+    return diameter * PI;
+  }
+
+  export function calculateRectangle(width: number, height: number) {
+    return width * length;
+  }
+}
